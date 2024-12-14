bf9c118a6c066ba77808ba1c760853a2bb889c69
RitankJaikar <ritank.jaikar@gmail.com>
2024-12-14 13:40:13 +0530

    Submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name, setName] = useState("");
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <div>Enter your name:</div>
+        <input type="text" value={name} onChange={e => setName(e.target.value)} />
+        <div>{name ? "Hello " + name : ""}</div>
     </div>
   )
 }
 
-export default App
+export default App;
